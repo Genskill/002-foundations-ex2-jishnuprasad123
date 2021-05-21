66e18aabc5eadb647c5681827a742c14733b6f59
jishnuprasad123 <84275082+jishnuprasad123@users.noreply.github.com>
2021-05-21 13:42:09 +0530

    Update pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <cs50.h>
-int main (void){
- int a = get_int("side 1 ");
- int b = get_int("side 2 ");
- int c = get_int("side 3 ");
- float d = (a*a)+(b*b);
- float e = (c*c);
- if (d==e){
- printf("Yes\n");
- }
- else{
- printf("No\n");
- }
 
+int main(){
+    float a,b,c,small_1,small_2,large;
+
+    a = get_float("side 1 ");
+    b = get_float("side 2 ");
+    c = get_float("side 3 ");
+    if (a<b){
+        if (b<c){
+            small_1 = a;
+            small_2 = b;
+            large = c;
+        }else{
+            small_1 = a;
+            small_2 = c;
+            large = b;
+        }
+    }
+    else{
+        if (a<c){
+            small_1 = b;
+            small_2 = a;
+            large = c;
+        }
+        else{
+            small_1 = b;
+            small_2 = c;
+            large = a;
+        }
+
+    }
+    if (small_1 * small_1 + small_2 * small_2 == large*large){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
 }
